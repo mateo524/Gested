257a3ccbb511f6570fc90bf5c807f45521af920d
Mateo <sanchezmateo113@gmail.com>
2026-04-23 00:02:42 -0300

    frontend preparado para produccion

--- proyecto-hr/src/pages/LoginPage.jsx
+++ proyecto-hr/src/pages/LoginPage.jsx
@@ -13,7 +13,7 @@ export default function LoginPage() {
     try {
       setMessage("");
 
-      const response = await fetch("http://localhost:3000/auth/login", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
